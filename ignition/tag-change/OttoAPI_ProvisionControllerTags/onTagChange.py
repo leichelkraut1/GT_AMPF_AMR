ae6132ab541e964d8147f03af1e21a0f0f366c73
leichelkraut <leichelkraut@factoryautomation.com>
2026-04-23 01:35:59 -0400

    New Script

---
+++ ignition/tag-change/OttoAPI_ProvisionControllerTags/onTagChange.py
@@ -0,0 +1,6 @@
+def onTagChange(initialChange, newValue, previousValue, event, executionCount):
+	"""Handle Ignition tag-change event for this resource."""
+	if system.tag.read("[Otto_FleetManager]Fleet/Triggers/SystemUpdates/ProvisionControllerTags").value == True:
+	    import MainController.State.Provisioning
+	    system.tag.writeAsync("[Otto_FleetManager]Fleet/Triggers/SystemUpdates/ProvisionControllerTags", 0)
+	    MainController.State.Provisioning.ensureRuntimeTags()
