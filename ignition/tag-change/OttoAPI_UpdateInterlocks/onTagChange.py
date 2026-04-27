3c910731b001a2e04ee8b1119b884287e174e04b
leichelkraut <leichelkraut@factoryautomation.com>
2026-04-27 16:54:43 -0400

    spaces to tabs

--- ignition/tag-change/OttoAPI_UpdateInterlocks/onTagChange.py
+++ ignition/tag-change/OttoAPI_UpdateInterlocks/onTagChange.py
@@ -1,5 +1,5 @@
 def onTagChange(initialChange, newValue, previousValue, event, executionCount):
-    if newValue.value == True:
-        #test
-        system.tag.writeAsync("[Otto_FleetManager]Fleet/Triggers/SystemUpdates/updateInterlocks", 0)
-        Otto_API.Interlocks.Runtime.runInterlockSyncCycle()
+	if newValue.value == True:
+		#test
+		system.tag.writeAsync("[Otto_FleetManager]Fleet/Triggers/SystemUpdates/updateInterlocks", 0)
+		Otto_API.Interlocks.Runtime.runInterlockSyncCycle()
