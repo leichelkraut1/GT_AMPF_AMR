d1391e704f2030bd09e57cebe6947765d9599b67
Luke <leichelkraut@factoryautomation.com>
2026-04-27 12:44:22 -0700

    From server

--- ignition/tag-change/OttoAPI_UpdateInterlocks/onTagChange.py
+++ ignition/tag-change/OttoAPI_UpdateInterlocks/onTagChange.py
@@ -1,8 +1,4 @@
 def onTagChange(initialChange, newValue, previousValue, event, executionCount):
-    """Handle Ignition tag-change event for the manual interlock update trigger."""
-    if bool(initialChange):
-        return
-
-    if bool(getattr(newValue, "value", False)):
+    if newValue.value == True:
         system.tag.writeAsync("[Otto_FleetManager]Fleet/Triggers/SystemUpdates/updateInterlocks", 0)
         Otto_API.Interlocks.Runtime.runInterlockSyncCycle()
